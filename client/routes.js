6a6cdab2d9d94d7fe53507428c080777b79e1c92
Carl Luo <cluo@jobs2careers.com>
2015-08-11 15:32:57 -0500

    it is working

--- client/routes.js
+++ client/routes.js
@@ -33,8 +33,17 @@ angular
       }]);
 
 angular
-      .module('favorite').controller('PlacesDetailController', ['$scope','$stateParams',
-        function($scope, $stateParams){
-          $scope.placeId = $stateParams.placeId;
+      .module('favorite').controller('PlacesDetailController', ['$scope','$stateParams','$meteor',
+        function($scope, $stateParams, $meteor){
+          $scope.item = $meteor.object(Place, $stateParams.placeId, false);
+
+          $scope.save = function(){
+            $scope.item.save();
+          };
+
+          $scope.reset = function(){
+            $scope.item.reset();
+          };
+
       }]);
 
