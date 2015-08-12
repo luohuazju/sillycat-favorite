9f3374fb5c8a5ccdeb2037f69617f369e48b8205
Carl Luo <cluo@jobs2careers.com>
2015-08-12 14:38:44 -0500

    this is a working copy

--- client/places/lib/app.js
+++ client/places/lib/app.js
@@ -1,2 +1,15 @@
 //load the modules
 angular.module('favorite',['angular-meteor', 'ui.router']);
+
+function onReady(){
+	angular.bootstrap(document, ["favorite"]);
+}
+
+if(Meteor.isCordova){
+	angular.element(document).on("deviceready", onReady);
+}else{
+	angular.element(document).ready(onReady);
+}
+
+var scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+Accounts.ui.config({'requestPermissions':{'google':scopes}});
