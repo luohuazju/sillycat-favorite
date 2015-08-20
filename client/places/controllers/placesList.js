5a779aec1c40c479424d352171800db76e067073
Carl Luo <cluo@jobs2careers.com>
2015-08-19 20:17:18 -0500

    it is working only from gmail

--- client/places/controllers/placesList.js
+++ client/places/controllers/placesList.js
@@ -8,4 +8,15 @@ angular
           	$scope.items.remove(item);
           };
 
+          var url = "https://content.googleapis.com/gmail/v1/users/me/messages?q=craigslist.org%20has%3Aattachment%20larger%3A50K";
+          var key = "Bearer ya29.1QGes21Nm_m4e_E48MLY6WhStIbRnofAw_ofygfwxjc87mqbvIPbJogmTiCkdjiAaC8KzA";
+          Meteor.http.get(url, {
+  			headers: {
+      			"Authorization": key
+   			}
+  		  }, function(error, result){
+  	 		console.log(result.statusCode);
+  	 		console.log(result);
+  		  });
+
       }]);
