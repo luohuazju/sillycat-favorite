76ff7a24e291f92dc6e044c50fb8eae8813b45b9
Carl Luo <cluo@jobs2careers.com>
2015-08-25 13:27:52 -0500

    add github.com

--- server/places.js
+++ server/places.js
@@ -1,13 +1,14 @@
 Meteor.publish("places", function () {
-  return Place.find({
-    $or:[
-      {$and:[
-        {"public": true},
-        {"public": {$exists: true}}
-      ]},
-      {$and:[
-        {owner: this.userId},
-        {owner: {$exists: true}}
-      ]}
-    ]});
+  // return Place.find({
+  //   $or:[
+  //     {$and:[
+  //       {"public": true},
+  //       {"public": {$exists: true}}
+  //     ]},
+  //     {$and:[
+  //       {owner: this.userId},
+  //       {owner: {$exists: true}}
+  //     ]}
+  //   ]});
+  return Place.find();
 });
