5850cc5fa86c21d63b8f01eea4fba924ad4a4cae
Carl Luo <cluo@jobs2careers.com>
2015-08-31 13:23:44 -0500

    login twice

--- client/index.js
+++ client/index.js
@@ -3,23 +3,24 @@
     //var firstUser = "";
     //var secondUser = "";
 
+    Template.registerHelper('session',function(input){
+        return Session.get(input);
+    }); 
+
     Template.content.events({
         "click #loginOther": function(e, tmpl){
             // var scopes = ['https://www.googleapis.com/auth/gmail.readonly',
             //               'https://www.googleapis.com/auth/userinfo.profile'];
             var scopes = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-            
-
             Meteor.loginWithGoogle({
                 requestPermissions: scopes,
-                forceApprovalPrompt: true,
-                loginHint: "luohuazju@gmail.com",
-                //userEmail: "luohuazju@gmail.com",
-                loginStyle: "redirect",
+                forceApprovalPrompt: false,
+                loginHint: "yiyikangrachel@gmail.com",
+                //loginStyle: "redirect",
+                loginStyle: "popup",
                 requestOfflineToken: true
             }, function(err) {
-                alert("second login.");
                 //console.log("recall from login second.");
                 if(err) {
                     //error handling
@@ -27,8 +28,10 @@
                     throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Error');
                 } else {
                     //show an alert
-                    alert('logged in');
-                    //console.log("second login .....");
+                    //alert('logged in');
+                    console.log("second login .....");
+                    var user2 = Session.get("previousUser");
+                    console.log("previous user = " + user2);
                 }
             });
 
@@ -51,21 +54,22 @@
 
             Meteor.loginWithGoogle({
                 requestPermissions: scopes,
-                forceApprovalPrompt: true,
+                forceApprovalPrompt: false,
                 loginHint: "cluo@jobs2careers.com",
                 //userEmail: "cluo@jobs2careers.com",
-                loginStyle: "redirect",
+                //loginStyle: "redirect",
+                loginStyle: "popup",
                 requestOfflineToken: true
             }, function(err) {
-                alert("first login");
                 if(err) {
                     //error handling
                     alert('error : '+ err);
                     throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Error');
                 } else {
                     //show an alert
-                    alert('logged in');
-                    //console.log("first login .....");
+                    //alert('logged in');
+                    console.log("first login .....");
+                    Session.set("previousUser", "cluo@jobs2careers.com");
                 }
             });
         }
@@ -78,7 +82,8 @@
                     //sow err message
                 } else {
                     //show alert that says logged out
-                    alert('logged out');
+                    //alert('logged out');
+                    console.log("logged out.");
                 }
             });
         }
---
+++ mobile-config.js
@@ -0,0 +1,1 @@
+App.accessRule('https://*.googleusercontent.com');
